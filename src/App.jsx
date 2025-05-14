03714a0640634a3707122d0a93ec4f6b4b44f511
danielemurgioni <dashomew@gmail.com>
2025-05-14 22:13:00 +0200

    [UPD] base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import { useState } from 'react'
+import axios from 'axios'
+
+const postEndpoint = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
 
 function App() {
 
   return (
     <>
-      <h1>react post form</h1>
+      <header>
+        <h1>React Post Form</h1>
+      </header>
+      <hr />
+      <main>
+        <form action="">
+          <div className='author-container'>
+            <label htmlFor="">Author:</label>
+            <input type="text" />
+          </div>
+          <div className='title-container'>
+            <label htmlFor="">Title:</label>
+            <input type="text" />
+          </div>
+          <div className='body-container'>
+            <label htmlFor="">Body:</label>
+            <input type="text" />
+          </div>
+          <div className='public-container'>
+            <input type="checkbox" />
+            <label htmlFor="">Check to Public</label>
+          </div>
+          <button type='submit'>Submit</button>
+        </form>
+      </main>
     </>
   )
 }
