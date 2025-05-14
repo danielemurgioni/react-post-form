c596a1d09fca2c8ae38507e0ee4b5269e4e88503
danielemurgioni <dashomew@gmail.com>
2025-05-15 00:15:10 +0200

    [UPD] set values to inputs

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ const postEndpoint = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
 
 function App() {
 
+  //variabile di stato oggetto, conterà i dati della form
+  const [formData, setFormData] = useState({
+    title: "",
+    author: "",
+    body: "",
+    public: false,
+  })
+
   return (
     <>
       <header>
@@ -13,21 +21,23 @@ function App() {
       <hr />
       <main>
         <form action="">
-          <div className='author-container'>
-            <label htmlFor="">Author:</label>
-            <input type="text" />
-          </div>
-          <div className='title-container'>
-            <label htmlFor="">Title:</label>
-            <input type="text" />
-          </div>
-          <div className='body-container'>
-            <label htmlFor="">Body:</label>
-            <input type="text" />
-          </div>
-          <div className='public-container'>
-            <input type="checkbox" />
-            <label htmlFor="">Check to Public</label>
+          <div className="inputs-container">
+            <div className='author-container'>
+              <label htmlFor="">Author:</label>
+              <input type="text" name='author' value={formData.author} />
+            </div>
+            <div className='title-container'>
+              <label htmlFor="">Title:</label>
+              <input type="text" name='title' value={formData.title} />
+            </div>
+            <div className='body-container'>
+              <label htmlFor="">Body:</label>
+              <input type="text" name='body' value={formData.body} />
+            </div>
+            <div className='public-container'>
+              <input type="checkbox" name='public' checked={formData.public} />
+              <label htmlFor="">Check to Public</label>
+            </div>
           </div>
           <button type='submit'>Submit</button>
         </form>
